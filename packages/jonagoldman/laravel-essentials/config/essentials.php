657a9d0040fa59ee38cd8adca94a984575e64c2e
Jona Goldman <jonagoldman@gmail.com>
2025-10-14 16:50:37 -0300

    feat: add service provider and default config

---
+++ packages/jonagoldman/laravel-essentials/config/essentials.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fake_sleep' => true,
+    'prevent_stray_requests' => true,
+    'force_https' => true,
+    'aggressive_prefetching' => true,
+    'immutable_dates' => true,
+    'model_unguard' => false,
+    'model_strict' => true,
+    'model_automatic_eager_load_relationships' => true,
+    'default_string_length' => 190,
+    'default_morph_key_type' => 'ulid',
+    'prohibit_destructive_commands' => true,
+    'set_default_passwords' => true,
+];
