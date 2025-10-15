eb30659f0c92085175ec6a8c0657ef5baa483f8d
Jona Goldman <jonagoldman@gmail.com>
2025-10-15 15:46:45 -0300

    refactor: add strict types, final classes, presets and minor cleanups

---
+++ tests/Unit/ArchitectureTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * It avoids the usage of die, var_dump, and similar functions, and ensures you are not using deprecated PHP functions.
+ */
+arch()->preset()->php();
+
+/**
+ * It ensures you are not using code that could lead to security vulnerabilities.
+ */
+arch()->preset()->security();
+
+/**
+ * It ensures the project structure is following well-known Laravel conventions.
+ */
+arch()->preset()->laravel();
+
+/**
+ * It ensures you are using strict types in all your files, that all your classes are final, and more.
+ */
+// arch()->preset()->strict();
