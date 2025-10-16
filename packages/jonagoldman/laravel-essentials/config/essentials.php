87bf9f9311a1f8d8468856150f402d5af1d49d76
Jona Goldman <jonagoldman@gmail.com>
2025-10-16 17:52:55 -0300

    feat: model dogma morph map

--- packages/jonagoldman/laravel-essentials/config/essentials.php
+++ packages/jonagoldman/laravel-essentials/config/essentials.php
@@ -11,31 +11,9 @@ return [
     'unguard_model' => false,
     'strict_model' => true,
     'automatic_eager_load_relationships' => true,
+    'require_morph_map' => true,
     'prohibit_destructive_commands' => true,
     'set_default_passwords' => true,
     'default_string_length' => 255,
     'default_morph_key_type' => 'int',
-
-    // 'model' => [
-    //     'unguard' => false,
-    //     'strict' => true,
-    //     'automatic_eager_load_relationships' => true,
-    // ],
-
-    // 'table' => [
-    //     'default_string_length' => 255,
-    //     'default_morph_key_type' => 'int',
-    // ],
-
-    // 'http' => [
-    //     'fake_sleep' => true,
-    //     'force_https' => true,
-    //     'prevent_stray_requests' => true,
-    //     'aggressive_prefetching' => true,
-    // ],
-
-    // 'general' => [
-    //     'prohibit_destructive_commands' => true,
-    //     'set_default_passwords' => true,
-    // ],
 ];
