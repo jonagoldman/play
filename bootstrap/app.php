e29b7999148f63abfc6e2cd6ad0f9801e0e77921
Jona Goldman <jonagoldman@gmail.com>
2025-10-20 14:49:15 -0300

    feat(essentials): add header guards and request id middlewares for security and tracing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use JonaGoldman\Essentials\Middlewares\UseHeaderGuards;
+use JonaGoldman\Essentials\Middlewares\UseRequestId;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->prependToGroup('web', [
+            UseRequestId::class,
+            UseHeaderGuards::class,
+        ]);
+
+        $middleware->prependToGroup('api', [
+            UseRequestId::class,
+            UseHeaderGuards::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
