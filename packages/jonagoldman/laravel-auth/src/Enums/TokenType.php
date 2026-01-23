d4e9af42bf656caff9f1e7834f4b2e806ec9e3f5
Jona Goldman <jonagoldman@gmail.com>
2026-01-23 15:06:30 -0300

    feat(laravel-auth): add Token model and session auth middleware to manage

---
+++ packages/jonagoldman/laravel-auth/src/Enums/TokenType.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JonaGoldman\Auth\Enums;
+
+use Illuminate\Support\Str;
+
+enum TokenType: string
+{
+    case BEARER = 'bearer';
+    case REMEMBER = 'remember';
+
+    public function generate(): string
+    {
+        return match ($this) {
+            self::BEARER => sprintf('%s%s', $secret = Str::random(40), hash('crc32b', $secret)),
+            self::REMEMBER => Str::random(60),
+        };
+    }
+}
